47fcd73abd5a0b86850ebb8683007fb5d57c2f5f
mrmodarres <85821449+mrmodarres@users.noreply.github.com>
2024-04-29 13:03:37 +0330

    fix bug for deploy bug was in api-client

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { HotelFormType, RegisterFormData, SignInFormType } from "./tying";
+import { RegisterFormData, SignInFormType } from "./tying";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
 export const registerApi = async (formData: RegisterFormData) => {
